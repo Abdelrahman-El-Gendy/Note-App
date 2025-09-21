8bfe6847c5d9a41d58abd6f35a198315a1297362
Abdelrahman Elgendy <73541296+Abdelrahman-El-Gendy@users.noreply.github.com>
2025-09-21 11:38:29 +0300

    add image removal functionality to note list items and view model to handle deleting images from storage

--- app/src/main/java/com/example/noteapp/feature_note/presentstion/notes/NotesEvent.kt
+++ app/src/main/java/com/example/noteapp/feature_note/presentstion/notes/NotesEvent.kt
@@ -6,6 +6,7 @@ import com.example.noteapp.feature_note.domain.util.NoteOrder
 sealed class NotesEvent {
     data class Order(val noteOrder: NoteOrder) : NotesEvent()
     data class DeleteNote(val note: Note) : NotesEvent()
+    data class RemoveImage(val note: Note) : NotesEvent()
     object RestoreNote : NotesEvent()
     object ToggleOrderSection : NotesEvent()
 
